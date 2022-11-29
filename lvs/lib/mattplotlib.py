780a5bab919b384f1e3d8af74c2fdb332aa3cdee
Matt <mattjac@umd.edu>
2022-11-29 12:01:46 -0500

    Created Xstrength and Xstrength_onehot features to add into the model as one option. Rearranged stuff a little and trained models to compare different experimental vars.

--- lvs/lib/mattplotlib.py
+++ lvs/lib/mattplotlib.py
@@ -3,15 +3,19 @@
 import matplotlib.pyplot as plt
 import NDNT.utils as utils # some other utilities
 import math
+import networkx as nx
+import NDNT.NDNT as NDN
 
 
 # make new figure for simplicity
-def fig():
+def fig(w=20, h=10):
     # close all figures if we have too many to avoid memory leak
     if plt.gcf().number > 20:
         plt.close("all")
     # make a new figure
-    return plt.figure()
+    figure = plt.figure()
+    figure.set_size_inches(w, h)  # width X height in inches
+    return figure
 
 
 # make a bunch of subplots of the provided vectors, and their names
@@ -57,5 +61,43 @@ def plot_glm_filters(glm, nc):
     plt.show()
 
 
+# use networkx to draw the network to see what it looks like
+def draw_model(model, names):
+    # TODO: go through each network, and its layers and connect them into a network
+    # TODO: add any attributes to the nodes and edges that make it clearer what they are doing
+    assert type(model) == NDN.NDN, "model needs to be of type NDN"
 
+    # define the network
+    g = nx.DiGraph()
 
+    # create the labels dictionary
+    labels = {idx: name for idx, name in enumerate(names)}
+
+    # go through the networks in the model
+    for netidx, network in enumerate(model.networks):
+        # add the network as a node
+        g.add_node(netidx)
+
+        # add edges between data and networks
+        if network.xstim_n is not None:
+            g.add_edge(network.xstim_n, netidx)
+            # add the stim name to the map of node labels
+            labels[network.xstim_n] = network.xstim_n
+
+        # add edges between networks
+        if network.ffnets_in is not None:
+            for inputidx in network.ffnets_in:
+                g.add_edge(inputidx, netidx)
+                # attribute the edge if the network type is something
+                # TODO: think about how to represent the attrs,
+                #       but don't get too deep into this yet.
+                #       Maybe I can use D3 for this to make it render nicer.
+                if network.network_type is not None:
+                    g[inputidx][netidx]['type'] = network.network_type
+        # inputs to each network
+        # QUESTION: can a network have ffnets_in and xstim_n?
+
+    # draw the network
+    # TODO: fix the layout to draw an inverted tree
+    # nx.draw_networkx(g, pos=hierarchy_pos(g))
+    nx.draw_networkx(g, labels=labels, with_labels=True)
