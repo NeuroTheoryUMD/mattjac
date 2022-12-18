Got the autoencoder working with the HN data. Trying to look at the r2 between the real firing rates and predicted firing rates across trials. TODO: I am doing something incorrect when normalizing the spiking data, and so the scale of the predicted are off. TODO: I am using ADAM instead of LBFGS, and I am not exactly sure how to configure LBFGS correctly to actually use it.

lvs/experiments/autoenc_hn_121822.py
@@ -1,18 +1,23 @@
 #%% Define Stuff
-__author__ = 'SherlockLiao'
+
+# From:
+# https://github.com/L1aoXingyu/pytorch-beginner/blob/master/08-AutoEncoder/simple_autoencoder.py
+# Explanation of backward functions
+# https://discuss.pytorch.org/t/what-does-the-backward-function-do/9944
 
 import os
-
 import sys
 # not best practice, but makes it easier to import from subdirectory
 sys.path.insert(0, '../lib')
 
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 import math
 import torch
 import torchvision
 from torch import nn
+from torch.nn import functional as F
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -22,49 +27,53 @@ import numpy as np
 
 import utils as ut
 
+# use Apple Silicon GPU (Metal Performance Shaders)
 device = torch.device("mps")
 
+# load the data
 data = ut.load_data('ki_0503_V2b_py.mat')
+# determine minimim trial size to truncate the trial lengths to
+# it is 117 in this case
+min_trial_size = min([len(block_ind) for block_ind in data.block_inds])
 
-num_epochs = 25
-batch_size = 16  # use a nice factor of 848 to avoid padding and stuff
+# params for training
+num_epochs = 20
+batch_size = 16  # use a factor of 848 to avoid padding and stuff
 learning_rate = 1e-3
 
-img_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])
-])
+# utility to get the batch of robs for a trial and format it correctly
+def get_batch(start_block_idx, batch_size, normalize=True):
+    """
+    Returns the batch from the starting block index (trial index),
+    up to batch_size subsequent trials afterward (stacked in an array). 
+    :param start_block_idx: starting block (trial) index
+    :param batch_size: number of subsequent trials to stack
+    :return: batch_size X 3627 array
+    """
+    # create the empty batch tensor
+    batch = torch.zeros((batch_size, min_trial_size*data.NC)).to(device)
+    # go through the timepoints for the block and accumulate the robs
+    #print(start_block_idx, start_block_idx+batch_size)
+    for row_idx, block_idx in enumerate(range(start_block_idx, start_block_idx+batch_size)):
+        batch[row_idx, :] = data[data.block_inds[block_idx]]['robs'][0:117,:].reshape(3627)
 
-dataloader = DataLoader(data, batch_size=batch_size, num_workers=4, shuffle=True)
+    # normalize the batch, if told to
+    if normalize:
+        batch = F.normalize(batch, dim=0)
+    return batch
 
-
-class autoencoder(nn.Module):
+# our AutoEncoder class
+class AutoEncoder(nn.Module):
     def __init__(self):
-        super(autoencoder, self).__init__()
+        super(AutoEncoder, self).__init__()
         self.encoder = nn.Sequential(
             # 3627 is the number of timepoints in a trial * the number of cells
             nn.Linear(3627, 2048),
-            nn.ReLU(True),
-            nn.Linear(2048, 512),
-            nn.ReLU(True),
-            nn.Linear(512, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 64),
-            nn.ReLU(True),
-            # use 3 latents
-            nn.Linear(64, 3))
+            # use 128 latents
+            nn.Linear(2048, 128))
         self.decoder = nn.Sequential(
-            nn.Linear(3, 64),
-            nn.ReLU(True),
-            nn.Linear(64, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 512),
-            nn.ReLU(True),
-            nn.Linear(512, 2048),
-            nn.ReLU(True),
-            nn.Linear(2048, 3627),
-            # to allow for negative values in the output img
-            nn.Tanh())
+            nn.Linear(128, 2048),
+            nn.Linear(2048, 3627))
 
     def forward(self, x):
         x = self.encoder(x)
@@ -72,35 +81,99 @@ class autoencoder(nn.Module):
         return x
     
 #%% Training Loop
-min_trial_size = min([len(block_ind) for block_ind in data.block_inds])
-
-model = autoencoder().to(device)
+model = AutoEncoder().to(device)
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(
-    model.parameters(), lr=learning_rate, weight_decay=1e-5)
+#optimizer = torch.optim.LBFGS(model.parameters(), history_size=10, max_iter=4)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
 
 losses = []
-for epoch in range(num_epochs):
-    print('epochs '+str(epoch))
+for epoch in tqdm(range(num_epochs)):
     # iterate over batches (927 batches)
     for batch_idx, start_block_idx in enumerate(range(0, len(data.block_inds), batch_size)):
-        #print('batch '+str(batch_idx))
-        # create the empty batch tensor
-        batch = torch.zeros((batch_size, min_trial_size*data.NC)).to(device)
-        # go through the timepoints for the block and accumulate the robs
-        #print(start_block_idx, start_block_idx+batch_size)
-        for row_idx, block_idx in enumerate(range(start_block_idx, start_block_idx+batch_size)):
-            batch[row_idx, :] = data[data.block_inds[block_idx]]['robs'][0:117,:].reshape(3627)
-            
-        # ===================forward=====================
-        output = model(batch)
-        loss = criterion(output, batch)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    # ===================log========================
-    print(loss.data.cpu())
+        # get the batch
+        batch = get_batch(start_block_idx, batch_size, normalize=True)
+        # forward
+        output = model(batch) # predict
+        loss = criterion(output, batch) # calculate the loss
+        # backward
+        optimizer.zero_grad() # zero the gradients
+        loss.backward() # calculate the gradient
+        optimizer.step() # update x with the gradient
+    # store losses
     losses.append(loss.data.cpu())
 
+# show the losses at the end of training
+plt.figure()
+plt.plot(losses)
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.show()
+
+#%% get the latents and plot the 2D manifold with UMAP
+batch = get_batch(0, 848)
+latents = model.encoder(batch).cpu().detach().numpy() # latents
+
+from sklearn.preprocessing import StandardScaler
+import umap
+
+reducer = umap.UMAP()
+scaled_latents = StandardScaler().fit_transform(latents)
+
+embedding = reducer.fit_transform(scaled_latents)
+print(embedding.shape)
+
+scat = plt.scatter(
+    embedding[:,0],
+    embedding[:,1],
+    c=data.TRcued) # color by cued trials
+plt.colorbar(scat) # show the colorbar (like a legend, but for colors)
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('UMAP projection of the latents')
+
+# calculate r2 between predicted and actual robs
+#reals = batch
+#preds = model.decoder(torch.tensor(latents).to(device))
+
+#mean_reals = torch.mean(reals, axis=0)
+
+# I don't think this is right
+#r2 = torch.sum((reals - preds)**2, axis=0) / torch.sum((reals - mean_reals)**2, axis=0)
+#print(r2)
+# r2 = sumSquaredRegression / totalSumOfSquares
+# sumSquaredRegression = sum((actual - predicted)**2)
+# totalSumOfSquared = sum((actual - mean_actual)**)
+
+
+#%% from Problem Set 7
+# r2 = 1 - (var(ratesR-predR') / var(ratesR))
+
+dt = 1.0/120.0  # bin time
+
+# get all robs
+reals = get_batch(0, 848, normalize=False)
+batch = get_batch(0, 848, normalize=True)
+# encode and decode to see if the autoencoder reproduces the input correctly
+latents = model.encoder(batch)
+preds = model.decoder(latents)
+
+# reshape the data into trials x time x robs
+reals = batch.reshape(848,117,31).cpu().detach().numpy()
+preds = preds.reshape(848,117,31).cpu().detach().numpy()
+
+# calculate firing rates from the reals and preds
+# calculate the trial-averaged rates (divide by dt to get it in terms of Hz
+# NOTE: this is incorrect, as we cannot try to predict the Hz from the normalized robs,
+#       we need to make sure real robs and pred robs are scaled correctly
+ratesR = np.mean(reals[:,:,0], axis=0) / dt
+ratesP = np.mean(preds[:,:,0], axis=0) / dt
+
+# calculate r2
+r2 = 1 - (np.var(ratesR-ratesP) / np.var(ratesR))
+
+# the first 3 points or so are not very good, so remove them from the plot
+plt.plot(ratesP[3:], label='pred rates')
+plt.plot(ratesR[3:], label='real rates')
+plt.legend()
+
+print('r2 = '+str(r2))
 #%%