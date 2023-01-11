Fixed the mattric r2 calculation. Adding multidataset cascade as well. Added visualization code using d3 as well.

v1/mattrics.py
@@ -1,21 +1,15 @@
-from sklearn.metrics import r2_score
+import numpy as np
 
 
-def r2(data, model, test_inds=None, variance_weighted=False):
+def r2(data, model, test_inds=None):
     # run the stimulus through the model and compare with the robs
     # run the stim forward through the model
-    if test_inds: # use only the provided test indices
-        y_pred = model({'stim': data.stim[test_inds]}).detach().numpy()
-        y_test = data.robs[test_inds].detach().numpy()
+    if test_inds is not None: # use only the provided test indices
+        y_pred = (model({'stim': data.stim[test_inds]})  * data.dfs[test_inds]).detach().numpy()
+        y_test = (data.robs[test_inds] * data.dfs[test_inds]).detach().numpy()
     else: # use all the data
-        y_pred = model({'stim': data.stim}).detach().numpy()
-        y_test = data.robs.detach().numpy()
+        y_pred = (model({'stim': data.stim}) * data.dfs).detach().numpy()
+        y_test = (data.robs * data.dfs).detach().numpy()
     
-    # set the flag
-    if variance_weighted: 
-        multioutput = 'variance_weighted'
-    else:
-        multioutput = 'raw_values'
-    
-    return r2_score(y_test, y_pred, multioutput=multioutput)
+    return 1 - np.sum((y_pred - y_test)**2, axis=0) / np.sum((y_test - np.mean(y_test))**2, axis=0)
 