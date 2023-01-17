874de05c428d7afb48b9986d63d5a65b41cfb969
matt <mattjac@umd.edu>
2023-01-17 17:20:49 -0500

    Added function to plot the NIM model layers.

--- v1/mattplotlib.py
+++ v1/mattplotlib.py
@@ -1,9 +1,10 @@
 # utilities to make plotting data simpler and easier
 
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import NDNT.utils as utils # some other utilities
 import math
-import string
 import networkx as nx
 import NDNT.NDNT as NDN
 
@@ -106,3 +107,61 @@ def draw_model(model, names=None):
     # TODO: fix the layout to draw an inverted tree
     # nx.draw_networkx(g, pos=hierarchy_pos(g))
     nx.draw_networkx(g, labels=labels, with_labels=True)
+
+
+
+def plot_layers(layers):
+    # plot receptive field at intermediate layers (weighted average of previous layers per column)
+    plt.style.use('dark_background')
+    
+    shapes = [(1,8), (1,11)]
+    for l in range(len(layers)): # go through each layer
+        layer = layers[l]
+        num_rows,num_cols = shapes[l]
+    
+        # multiply the previous layer features by the subunit's weighting of it
+        if l == 0: # plot the first layer
+            fig, axes = plt.subplots(num_rows,num_cols, figsize=(num_cols*2,num_rows*2))
+            fig.suptitle('layer ' + str(l))
+    
+            layer = np.swapaxes(layer, 0,1) # rotate the image
+            #g = isns.ImageGrid(layer, axis=len(layer.shape)-1, cbar=False, col_wrap=cols[l], cmap='inferno', vmax=imax, vmin=imin)
+            for sub in range(layer.shape[-1]): # go through each subunit
+                ax = axes.flatten()[sub]
+                # to index the last axis for arrays with any number of axes
+                subunit = layer.take(indices=sub, axis=len(layer.shape)-1) # equivalent to layer[:,:,sub]
+                imin = np.min(subunit.flatten())
+                imax = np.max(subunit.flatten())
+                #isns.imshow(subunit, ax=axes.flatten()[sub], cbar=False, vmin=imin, vmax=imax)
+                ax.set_axis_off() # remove axis
+                ax.imshow(subunit, vmin=imin, vmax=imax, aspect='auto', cmap='inferno')
+    
+        else:
+            fig, axes = plt.subplots(num_rows+1,num_cols,
+                                     figsize=(num_cols*2,(num_rows+1)*1.2),
+                                     gridspec_kw={'height_ratios': [1, 5]})
+            fig.suptitle('layer ' + str(l))
+    
+            # plot the subunit weights
+            axidx = 0
+            for sub in range(layer.shape[1]): # for each subunit
+                ax = axes.flatten()[axidx]
+                weights = np.expand_dims(layer[:,sub], 0)
+                imin = np.min(weights.flatten())
+                imax = np.max(weights.flatten())
+                ax.set_axis_off() # remove axis
+                ax.imshow(weights, vmin=imin, vmax=imax, cmap='inferno')
+                axidx += 1
+    
+            # plot the subunit weightings in the subsequent layers
+            for sub in range(layer.shape[1]): # for each subunit
+                ax = axes.flatten()[axidx]
+                weighted_sub = np.mean(layer[:,sub] * layers[l-1], axis=len(layers[l-1].shape)-1)
+                weighted_sub = np.swapaxes(weighted_sub, 0,1)
+                imin = np.min(weighted_sub.flatten())
+                imax = np.max(weighted_sub.flatten())
+                ax.set_axis_off() # remove axis
+                ax.imshow(weighted_sub, vmin=imin, vmax=imax, aspect='auto', cmap='inferno')
+                axidx += 1
+    
+    plt.show()
